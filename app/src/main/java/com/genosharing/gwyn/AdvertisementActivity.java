463bf565ea3924916c15e8bcc0e09b57a3dd2e74
Sweet <abc@xyz.de>
2016-12-06 15:15:46 +0100

    second commit

---
+++ app/src/main/java/com/genosharing/gwyn/AdvertisementActivity.java
@@ -0,0 +1,19 @@
+package com.genosharing.gwyn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sweet on 06.12.16.
+ */
+public class AdvertisementActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ad_activity);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/genosharing/gwyn/StartActivity.java
@@ -0,0 +1,15 @@
+package com.genosharing.gwyn;
+
+import android.app.Activity;
+
+/**
+ * Created by Sweet on 22.10.16.
+ */
+public class StartActivity extends Activity {
+
+
+
+
+
+}
+
