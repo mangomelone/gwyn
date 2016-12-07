d0a15ee3f03c719fb7837cbeedf0444f539b5a8e
Sweet <abc@xyz.de>
2016-12-07 11:07:01 +0100

    first activtiy completed

--- app/src/main/java/com/genosharing/gwyn/AdvertisementActivity.java
+++ app/src/main/java/com/genosharing/gwyn/AdvertisementActivity.java
@@ -1,6 +1,8 @@
 package com.genosharing.gwyn;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -14,9 +16,20 @@ public class AdvertisementActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ad_activity);
 
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.slogan);
+        Bitmap resizedSlogan = Bitmap.createScaledBitmap(bm, (int) (bm.getWidth() * 0.75), (int) (bm.getHeight() * 0.75), true);
         ImageView sloganImage = (ImageView) findViewById(R.id.imageView);
-        sloganImage.setImageResource(R.drawable.slogan);
-        sloganImage.bringToFront();
+        sloganImage.setImageBitmap(resizedSlogan);
+
+        Bitmap bm2 = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+        Bitmap resized = Bitmap.createScaledBitmap(bm2, (int) (bm2.getWidth() * 0.6), (int) (bm2.getHeight() * 0.6), true);
+        ImageView logoImage = (ImageView) findViewById(R.id.logoImageView);
+        logoImage.setImageBitmap(resized);
+
+        Bitmap bm3 = BitmapFactory.decodeResource(getResources(), R.drawable.btnfinden);
+        Bitmap resizedBtn = Bitmap.createScaledBitmap(bm3, (int) (bm3.getWidth() * 0.5), (int) (bm3.getHeight() * 0.5), true);
+        ImageView buttonImage = (ImageView) findViewById(R.id.btnimageView);
+        buttonImage.setImageBitmap(resizedBtn);
 
 
     }
