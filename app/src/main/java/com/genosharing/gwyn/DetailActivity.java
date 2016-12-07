f437d369500c21cf76c94d435440dea49713b332
Sweet <abc@xyz.de>
2016-12-07 12:27:39 +0100

    hüpfburg and stefan added

---
+++ app/src/main/java/com/genosharing/gwyn/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.genosharing.gwyn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sweet on 07.12.16.
+ */
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
