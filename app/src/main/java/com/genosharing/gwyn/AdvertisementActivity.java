a77f47f4025a1b388f67fa50265f16cc8e289802
Sweet <abc@xyz.de>
2016-12-07 09:39:54 +0100

    third commit

--- app/src/main/java/com/genosharing/gwyn/AdvertisementActivity.java
+++ app/src/main/java/com/genosharing/gwyn/AdvertisementActivity.java
@@ -2,6 +2,7 @@ package com.genosharing.gwyn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 /**
  * Created by Sweet on 06.12.16.
@@ -12,6 +13,10 @@ public class AdvertisementActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ad_activity);
+
+        ImageView sloganImage = (ImageView) findViewById(R.id.imageView);
+        sloganImage.setImageResource(R.drawable.slogan);
+        sloganImage.bringToFront();
 
 
     }
